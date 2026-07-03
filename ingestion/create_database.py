e472b18cb2a2bfd001244b65af5fc0e49313f430
Seow How <seowhow@gmail.com>
2026-07-03 18:03:21 +0800

    updated create_database.py

--- ingestion/create_database.py
+++ ingestion/create_database.py
@@ -2,7 +2,7 @@
 import sqlite3
 from pathlib import Path
 
-DB_PATH = Path("documentsDB.db")
+DB_PATH = Path("sqlite/documentsDB.db")
 
 
 def create_database():
@@ -13,27 +13,43 @@ def create_database():
     # create Documents table if absent
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS documents (
-        doc_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        doc_id TEXT PRIMARY KEY,
         doc_name TEXT NOT NULL,
         doc_path TEXT NOT NULL UNIQUE,
         doc_type TEXT NOT NULL,
-        vendor TEXT NOT NULL
+        vendor_name TEXT NOT NULL
     )
     """)
 
-    # create page text table if absent
-    ## need to redo this part, plus also look through what other fields to use based on chatgpt inputs
+    # create Pages table if absent
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS pages (
+        doc_id TEXT NOT NULL,
+        page_number INTEGER NOT NULL,
+        start_offset INTEGER NOT NULL,
+        end_offset INTEGER NOT NULL,
+        page_text TEXT,
+                   
+        PRIMARY KEY (doc_id, page_number),
+
+        FOREIGN KEY (doc_id)
+            REFERENCES documents(doc_id)
+            ON DELETE CASCADE           
+    )
+    """)
     
     # create Text chunks table if absent
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS chunks (
         chunk_id TEXT PRIMARY KEY, 
         chunk_text TEXT NOT NULL,           
-        doc_id INTEGER NOT NULL,
+        doc_id TEXT NOT NULL,
         section TEXT,
-        chunk_page_start INTEGER,
-        chunk_page_end INTEGER,
-        simhash INTEGER,
+        chunk_start INTEGER NOT NULL,
+        chunk_end INTEGER NOT NULL,
+        page_start INTEGER NOT NULL,
+        page_end INTEGER NOT NULL,
+        simhash TEXT NOT NULL,
 
         FOREIGN KEY (doc_id)
             REFERENCES documents(doc_id)
@@ -45,16 +61,23 @@ def create_database():
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS images (
         image_id TEXT PRIMARY KEY,
-        image_path TEXT NOT NULL,
-        doc_id INTEGER NOT NULL,
-        page_slide_number INTEGER,
-        phash TEXT,
+        image_path TEXT NOT NULL UNIQUE,
+        doc_id TEXT NOT NULL,
+        page_number INTEGER NOT NULL,
+        phash TEXT NOT NULL,
 
         FOREIGN KEY (doc_id)
             REFERENCES documents(doc_id)
             ON DELETE CASCADE
     )
     """)
+
+    # Create indexes to facilitate database search
+
+    cursor.execute("""
+    CREATE INDEX IF NOT EXISTS idx_chunks_doc ON chunks(doc_id)""")
+    cursor.execute("""
+    CREATE INDEX IF NOT EXISTS idx_images_doc ON images(doc_id)""")
 
     # Check that tables are successfully created
     cursor.execute("""SELECT name FROM sqlite_master WHERE type='table' """)
